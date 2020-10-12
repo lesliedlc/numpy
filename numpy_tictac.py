f09dc27e370affc2d59b960e289a787431bf9f3c
Posada De La Cruz <Leslie_PosadaDeLaCr1@baylor.edu>
2020-10-12 16:16:55 -0500

    start

---
+++ numpy_tictac.py
@@ -0,0 +1,10 @@
+#1 [[,,],[,,],[,,]]
+#2 
+#3
+# output: [[],[,x,],[]] --array
+#5 ex: 
+'''
+count_row = np.count_nonzero(myarray == 'X')
+count_row = np.count_nonzero(myarray == 'X')
+count_row = np.count_nonzero(myarray == 'X')
+'''
