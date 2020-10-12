1c9b92277fcc767c0408de238047230cf0466662
Posada De La Cruz <Leslie_PosadaDeLaCr1@baylor.edu>
2020-10-12 16:28:07 -0500

    comments

--- numpy_tictac.py
+++ numpy_tictac.py
@@ -1,10 +1,22 @@
+import numpy as np
+import random
 #1 [[,,],[,,],[,,]]
-#2 
-#3
-# output: [[],[,x,],[]] --array
+#2 let user choose row, col num to put X
+#3 computer chooses random int 0-2 , if its already occpied, go to while board
+# output: [[O,,X],[X,X,],[O,O,]] --array
 #5 ex: 
 '''
-count_row = np.count_nonzero(myarray == 'X')
-count_row = np.count_nonzero(myarray == 'X')
-count_row = np.count_nonzero(myarray == 'X')
+array = np.array([[X,O,O],[X,O,X],[O,X,O]])
+count_row = np.count_nonzero(myarray == 'X') ##count all x in the whole array
+count_row = np.count_nonzero(myarray == 'X', axis = 1) #count x per row
+count_column = np.count_nonzero(myarray == 'X', axis = 0) #count x per column
+
+# for the last two, rows and columns, if any element in array = 3, then user wins, then repeat = false
+# while board[r,c] == 'x' or ''o', get row column again, until it finds a non occupied spot
+
+#put it on board
+# print board
+# get row count, column count for computer
+#if any element = 3, then comp wins, repeat = false
 '''
+
